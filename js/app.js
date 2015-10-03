8e1b4a2978e862930ac55b3b803d6e9c33df7d01
iampaul783 <iampaul783@gmail.com>
2015-10-03 11:42:01 +0800

    ADD controller

--- js/app.js
+++ js/app.js
@@ -3,4 +3,21 @@
         "hello-ng", // ng-app
         [] // module dependencies
     );
+
+    //data
+    var persons = [
+        {
+            name: "Paul",
+            age: 25
+        },
+        {
+            name: "Bonnie",
+            age: 25
+        }
+    ];
+
+    //controller
+    helloNg.controller("PersonListController", function() {
+        this.list = persons;
+    });
 })()
